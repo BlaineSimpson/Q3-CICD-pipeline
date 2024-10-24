eab199e495931407beab6d11825490d3c0aae83c
BlaineSimpson <BlaineSimpson@CTUASSET0020>
2024-10-24 10:08:15 +0200

    Add project files.

---
+++ Q3 CICD pipeline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Q3_CICD_pipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
